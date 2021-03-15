14bd83d07f45b8fe7ace8776c0a8cfa749ca022e
matkwon <62735876+matkwon@users.noreply.github.com>
2021-03-15 10:00:05 -0300

    begin

---
+++ VaiMalte/src/br/edu/mateluca/Carrinho.java
@@ -0,0 +1,5 @@
+package br.edu.mateluca;
+
+public class Carrinho {
+
+}
---
+++ VaiMalte/src/br/edu/mateluca/Testador.java
@@ -0,0 +1,25 @@
+package br.edu.mateluca;
+
+public class Testador {
+	
+	public boolean testeA() {
+	    return false;
+	}
+
+	public boolean testeB() {
+	    return false;
+	}
+
+	public boolean testeC() {
+	    return false;
+	}
+
+	public boolean testeD() {
+	    return false;
+	}
+
+	public boolean testeE() {
+	    return false;
+	}
+
+}
