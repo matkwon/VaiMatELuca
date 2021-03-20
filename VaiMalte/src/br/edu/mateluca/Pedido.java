cd9a13a05cfd050f852cb0407f901cdb31c36c73
lucamelao <63018319+lucamelao@users.noreply.github.com>
2021-03-20 18:34:21 -0300

    Classes produto e pedido
    Adicionei as classes produto.java e pedido.java ao projeto.

--- VaiMalte/src/br/edu/mateluca/Pedido.java
+++ VaiMalte/src/br/edu/mateluca/Pedido.java
@@ -1,5 +1,30 @@
 package br.edu.mateluca;
 
 public class Pedido {
+	
+	private Produto produto;
+	private int quantidade;
+	
+	public Pedido(Produto produto) {
+		super();
+		this.produto = produto; 
+		this.quantidade = 1;
+	}
 
+	public Produto getProduto() {
+		return produto;
+	}	
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void incremento() {
+		quantidade++;
+	}
+		
+	public double devolveTotal(this.produto.getPreço(), this.quantidade){
+		double total = produto.preço*quantidade;
+		return total;
+	}
 }
--- VaiMalte/src/br/edu/mateluca/Produto.java
+++ VaiMalte/src/br/edu/mateluca/Produto.java
@@ -1,5 +1,28 @@
 package br.edu.mateluca;
 
-public class Produto {
 
+public class Produto {
+	
+	private int codigo;
+	private String nome;
+	private double preço;
+	
+	public Produto(int codigo, String nome, double preço) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.preço = preço;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreço() {
+		return preço;
+	}
 }
